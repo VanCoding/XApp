c63fd1175082d46460bca23aa45f960930549e93
Patrik Stutz <patrik.stutz@gmail.com>
2011-11-08 15:23:11 +0100

    added first intent

--- src/cloudstudios/XApp/ConnectActivity.java
+++ src/cloudstudios/XApp/ConnectActivity.java
@@ -1,83 +1,34 @@
 package cloudstudios.XApp;
 
-import java.util.ArrayList;
-
+import cloudstudios.XClient.Client;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.SeekBar;
-import android.widget.SeekBar.OnSeekBarChangeListener;
-import cloudstudios.XClient.*;
 
-public class ConnectActivity extends ViewActivity implements OnSeekBarChangeListener {
-    /** Called when the activity is first created. */
+public class ConnectActivity extends ViewActivity implements OnClickListener {
+    public static Client client;
+    private Txt ip;
+    private Btn connect;
 	
-	private Lbl label;
-	private Txt ip;
-	private Btn connect;
-	private ArrayList<SeekBar> bars = new ArrayList<SeekBar>();
-	private ArrayList<Txt> fields = new ArrayList<Txt>();
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Lin m;
-        setContentView(
-        	Vs(
-        		m = Lin().vertical(true)
-        	)
-        );
         
-        for(int i = 0; i < 16; i++){
-        	m.addView(Channel(null));
-        }
-        
-        /*
-        setContentView(Lin(
-        	Lbl("IP:")
-        ));*/
-        /*
-        Lin l;
         setContentView(Vs(
-        	l = Lin(
-        		label = Lbl("IP:").width(100),
+        	Lin(
+        		Lbl("IP:").width(100),
         		ip = Txt("192.168.1.45").width(-1)
         	),
         	Lin(
         		connect = Btn("Connect").width(200)
         	).gravity(5)
         ));
-        connect.setOnClickListener(new OnClickListener(){
-        	public void onClick(View v){
-        		Log.d("geil","o");
-        	}
-        });*/
+        connect.setOnClickListener(this);
+        
     }
     
-    public Lin Channel(Channel c){
-    	Slider s;
-    	Txt t;
-    	Lin channel = Lin(
-    		Chk(),
-    		Lbl("IN 1").width(75),
-    		s = Slider().width(250),
-    		t = Txt("0").width(75).textsize(10)
-    		
-    	).gravity(16).height(50);
-    	
-    	bars.add(s);
-    	fields.add(t);
-    	t.setInputType(2);
-    	s.setOnSeekBarChangeListener(this);
-    	
-    	return channel;
-    }
-
-	public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
-		fields.get(bars.indexOf(seekBar)).setText(progress+"");
+    public void onClick(View v){
+    	client = new Client(ip.getText().toString(),0);
+		this.startActivity(new Intent(this,ControlActivity.class));
 	}
-
-	public void onStartTrackingTouch(SeekBar seekBar) {}
-	public void onStopTrackingTouch(SeekBar seekBar) {}
 }
