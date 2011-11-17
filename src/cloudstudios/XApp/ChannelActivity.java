310504dd0bf9634d5af8c4deef1b50bd251a56ac
Patrik Stutz <patrik.stutz@gmail.com>
2011-11-17 23:49:21 +0100

    added slider to set gain

--- src/cloudstudios/XApp/ChannelActivity.java
+++ src/cloudstudios/XApp/ChannelActivity.java
@@ -9,8 +9,8 @@ import cloudstudios.XClient.Channel;
 
 public class ChannelActivity extends ViewActivity implements OnCheckedChangeListener, OnSeekBarChangeListener {
 	
-	private Slider sDelay,sLevel;
-	private Lbl tDelay,tLevel;
+	private Slider sDelay,sLevel,sGain;
+	private Lbl tDelay,tLevel,tGain;
 	
 	
 	private Channel channel;
@@ -30,17 +30,23 @@ public class ChannelActivity extends ViewActivity implements OnCheckedChangeList
         	Lin(
 	        	Chk().checked(channel.getMute()).change(this),
 	        	Lbl("Mute").width(200)        
-	        ),
+	        ),	        
+	        Lbl("Level (dB)"),
+	        Lin(	        	
+	        	sLevel = Slider().width(0.8).max(220).seeked(this).progress(channel.getLevel()),
+	        	tLevel = Lbl("").width(100).textsize(12).padding(10)
+	        ).gravity(16),
 	        Lbl("Delay (ms)"),
 	        Lin(	        	
 	        	sDelay = Slider().width(0.8).max(62400).seeked(this).progress(channel.getDelay()),
 	        	tDelay = Lbl("").width(100).textsize(12).padding(10)
 	        ).gravity(16),
-	        Lbl("Level (dB)"),
+	        Lbl("Gain (dB)"),
 	        Lin(	        	
-	        	sLevel = Slider().width(0.8).max(220).seeked(this).progress(channel.getLevel()),
-	        	tLevel = Lbl("").width(100).textsize(12).padding(10)
+	        	sGain = Slider().width(0.8).max(15).seeked(this).progress(channel.getGain()),
+	        	tGain = Lbl("").width(100).textsize(12).padding(10)
 	        ).gravity(16)
+	        
         ));
         
         onProgressChanged(sDelay,channel.getDelay(),false);
@@ -54,6 +60,8 @@ public class ChannelActivity extends ViewActivity implements OnCheckedChangeList
 			tDelay.setText(Math.round((progress*100)/96.0)/100.0+"");
 		}else if(s == sLevel){
 			tLevel.setText(Math.round((progress*100)/4.0)/100.0-40.0+"");
+		}else if(s == sGain){
+			tGain.setText(progress*3+"");
 		}
 	}
 	public void onStartTrackingTouch(SeekBar seekBar) {}
@@ -62,6 +70,8 @@ public class ChannelActivity extends ViewActivity implements OnCheckedChangeList
 			channel.setDelayAsync(sDelay.getProgress());
 		}else if(s == sLevel){
 			channel.setLevelAsync(sLevel.getProgress());
+		}else if(s == sGain){
+			channel.setGainAsync(sGain.getProgress());
 		}
 	}
 }
